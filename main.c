2a259546b867009daf9e14597ad66f54981ab77d
Steven Solano <solanors20@gmail.com>
2020-02-20 13:53:57 -0600

    Se agrega parametros de cantidad de carros

--- main.c
+++ main.c
@@ -5,20 +5,25 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define EAST_THREAD_SIZE 2
-#define WEST_THREAD_SIZE 2
 #define EAST_DIRECTION 0
 #define WEST_DIRECTION 1
+#define name(x) ((x==1) ? ("WEST") : ("EAST"))
 
 int east = 0, west = 0;    // # of cars in each direction
 
 sem_t on_the_bridge;
 
+struct car_creation_thread {
+    int direction;
+    int size;
+    double medium;
+};
+
 void cross(int direction)
 {
 
-    if ((0 == direction && west) || (1 == direction && east)) {
-        if(0==direction)
+    if ((EAST_DIRECTION == direction && west) || (WEST_DIRECTION == direction && east)) {
+        if(EAST_DIRECTION==direction)
         {
             printf("waiting [<-] east=%d west=%d thread=%ld\n", east, west, pthread_self());
         } else {
@@ -29,7 +34,7 @@ void cross(int direction)
     }
 
 
-    if (0 == direction)
+    if (EAST_DIRECTION == direction)
     {
         east++;
         printf("(+) [<-] east=%d west=%d thread=%ld\n", east, west, pthread_self());
@@ -43,18 +48,17 @@ void cross(int direction)
 void leave(int direction)
 {
 
-    if (0 == direction && 0 < east) {
-
+    if (EAST_DIRECTION == direction && 0 < east) {
         --east;
-        printf("leave [<-] direction=%d east=%d west=%d thread=%ld\n", direction, east, west, pthread_self());
+        printf("leave [<-] direction=%s east=%d west=%d thread=%ld\n", name(direction), east, west, pthread_self());
         sleep(1);
         sem_post(&on_the_bridge);
     }
 
 
-    else if (1 == direction && 0 < west) {
+    else if (WEST_DIRECTION == direction && 0 < west) {
         --west;
-        printf("leave [->] direction=%d west=%d east=%d thread=%ld\n", direction, west, east, pthread_self());
+        printf("leave [->] direction=%s west=%d east=%d thread=%ld\n", name(direction), west, east, pthread_self());
         sleep(1);
         sem_post(&on_the_bridge);
     }
@@ -73,35 +77,53 @@ double ran_expo(double lambda){
     return -log(1- u) / lambda;
 }
 
-int main() {
+void *thread_creation(struct car_creation_thread *car_thread){
+    int quantity = 0;
+    pthread_t* direction_cars = malloc(sizeof(pthread_t) * car_thread->size);
+    while(car_thread->size > 0){        
+        pthread_create(&direction_cars[quantity], NULL, thread, car_thread->direction);
+        quantity--;        
+        car_thread->size--;
+        int random_number = (int)ran_expo(car_thread->medium);
+        sleep(random_number);
+    }
+    for (int i = 0; i < car_thread->size; ++i) {
+        pthread_join(direction_cars[i], NULL);
+    }
+    free(direction_cars);
+}
+
+int main(int argc, char *argv[]) {
+    int WEST_THREAD_SIZE = atoi(argv[1]);
+    int EAST_THREAD_SIZE  = atoi(argv[2]);
+
+    printf("West cars length: %i\n", WEST_THREAD_SIZE); 
+    printf("East cars length: %i\n", EAST_THREAD_SIZE); 
 
     sem_init(&on_the_bridge, 0, 1);
-    pthread_t east_cars[EAST_THREAD_SIZE], west_cars[WEST_THREAD_SIZE];
+
+    pthread_t* thread_creation_list = malloc(sizeof(pthread_t) * 2);
+    //pthread_t east_cars[EAST_THREAD_SIZE], west_cars[WEST_THREAD_SIZE];
     int thread_total = EAST_THREAD_SIZE + WEST_THREAD_SIZE;
-    int quantity_of_east = 0;
-    int quantity_of_west = 0;
+
     srand(time(NULL));
-    while(thread_total != 0){
-        pthread_create(&east_cars[quantity_of_east], NULL, thread, EAST_DIRECTION);
-        quantity_of_east++;
-        thread_total--;
-        int random_number = (int)ran_expo(0.7);
-        printf("random=%d", random_number);
-        sleep(random_number);
-        pthread_create(&west_cars[quantity_of_west], NULL, thread, WEST_DIRECTION);
-        quantity_of_west++;
-        thread_total--;
-    }
 
-    for (int i = 0; i < EAST_THREAD_SIZE; ++i) {
-        pthread_join(east_cars[i], NULL);
-    }
+    struct car_creation_thread thread_weast;
+    thread_weast.direction = WEST_DIRECTION;
+    thread_weast.size = WEST_THREAD_SIZE;
+    thread_weast.medium = 0.01;
+    struct car_creation_thread thread_east;
+    thread_east.direction = EAST_DIRECTION;
+    thread_east.size = EAST_THREAD_SIZE;
+    thread_east.medium = 0.07;
 
-    for (int j = 0; j < WEST_THREAD_SIZE; ++j) {
-        pthread_join(west_cars[j], NULL);
-    }
+    pthread_create(&thread_creation_list[0], NULL, thread_creation, &thread_weast);
+    pthread_create(&thread_creation_list[1], NULL, thread_creation, &thread_east);
+    pthread_join(thread_creation_list[0], NULL);
+    pthread_join(thread_creation_list[1], NULL);
 
     sem_destroy(&on_the_bridge);
+    free(thread_creation_list);
     printf("\n\n\nwest=%d,", west);
     printf("east=%d\n\n\n", east);
 
