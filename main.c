6e453413c46cc34f0f6599416845db3705d3d994
LuisCastilloV98 <lui19982011@gmail.com>
2020-02-25 05:57:01 +0100

    Progra terminada funcionan - faltan comentarios

--- main.c
+++ main.c
@@ -17,11 +17,22 @@ cars to the west will have the traffic light on 1*/
 
 
 int east = 0, west = 0;    // # of cars in each direction
-int east_pass = 0, west_pass = 0;    // # of cars in each direction
+int east_pass = 0, west_pass = 0;    // # Cada uno delos carros que estan en el puente
 int bridge_direction = -1;    // # of cars in each direction
 
+pthread_mutex_t mut_east = PTHREAD_MUTEX_INITIALIZER; //Semaforo para detener los del este
+pthread_mutex_t mut_west = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_cond_t con_east = PTHREAD_COND_INITIALIZER; //Condicionador para hacer brohgcast a los del este
+pthread_cond_t con_west = PTHREAD_COND_INITIALIZER;
+
+
 //Semaphore pointer
-sem_t sem_east, sem_west, screen, sem_order, sem_bridge;
+sem_t sem_real_bridge, screen, sem_order;
+//El primero es para ver el puente si está libre
+//Segundo para la impresion
+//El tercero es para control de las variables de control
+
 
 //Cars thread struct.
 struct car_creation_thread {
@@ -33,22 +44,29 @@ struct car_creation_thread {
 //Cross method will recieve est or west
 void cross(int direction)
 {   
+    int actual_east_pass = 0;
+    int actual_west_pass = 0;
+
     if (EAST_DIRECTION == direction)
     {   
+        //El carro creado es del este
         sem_wait(&sem_order);
         east++;
         sem_post(&sem_order);
 
         sem_wait(&screen);
         printf("\x1b[32;1m\x1b[1m(+) Cars created (EAST) [<-] east=%d west=%d thread=%ld\x1b[0m\n", east, west, pthread_self());
+        fflush(stdin); 
         sem_post(&screen);
     } else {
+        //El carro creado es del oeste
         sem_wait(&sem_order);
         west++;
         sem_post(&sem_order);
         
         sem_wait(&screen);
         printf("\x1b[32;1m\x1b[1m(+) Cars created (WEST) [->] west=%d east=%d thread=%ld\x1b[0m\n", west, east, pthread_self());
+        fflush(stdin); 
         sem_post(&screen);
     }
     
@@ -56,118 +74,133 @@ void cross(int direction)
     sleep(5);
 
     if(EAST_DIRECTION == direction)
-        {
-            //Cars waiting to pass (east).
-            sem_wait(&sem_order);
-            east_pass++;
+    {
+        //Se espera a que los carros lleguen al puente.
+        sem_wait(&sem_order);
+        east_pass++;
+        actual_east_pass = east_pass;
+        sem_wait(&screen);
+        printf("\x1b[0;34m\x1b[1m(...)East car arrive to the bridge [<-] east=%d west=%d east_pass=%d thread=%ld\x1b[0m\n", east, west, east_pass, pthread_self());
+        fflush(stdin); 
+        sem_post(&screen);
+        sem_post(&sem_order);
 
+        if (bridge_direction == WEST_DIRECTION){ //Direccion del puente es opuesta
             sem_wait(&screen);
-            printf("\x1b[0;34m\x1b[1m(...)East car arrive to the bridge [<-] east=%d west=%d east_pass=%d thread=%ld\x1b[0m\n", east, west, east_pass, pthread_self());
+            printf("\x1b[31;1m\x1b[1m(...)East waiting [<-] east_pass=%d  thread=%ld\x1b[0m\n", east_pass, pthread_self());
+            fflush(stdin); 
             sem_post(&screen);
-
-            sem_post(&sem_order);
-
-            if (bridge_direction == WEST_DIRECTION){
-                sem_wait(&screen);
-                printf("\x1b[31;1m\x1b[1m(...)East waiting [<-] east_pass=%d  thread=%ld\x1b[0m\n", east_pass, pthread_self());
-                sem_post(&screen);
-            }
-
-
-            sem_wait(&sem_east);
-
-            sem_wait(&sem_bridge);
-            if (bridge_direction == EAST_DIRECTION){
-                
-                sem_wait(&screen);
-                printf("\x1b[31;1m\x1b[1m(...)East (1) passing the bridge [<-] east_pass=%d thread=%ld\x1b[0m\n", east_pass, pthread_self());
-                sem_post(&screen);
-            
-            } else if (bridge_direction == -1 || east_pass == 1){
-                sem_wait(&sem_west);
-                bridge_direction = EAST_DIRECTION;
-                
-                sem_wait(&screen);
-                printf("\x1b[31;1m\x1b[1m(...)East start to pass [<-] east_pass=%d  thread=%ld\x1b[0m\n", east_pass, pthread_self());
-                sem_post(&screen);   
-            }
-            sem_post(&sem_bridge);
-
-        } else {
-            //Cars waiting to pass (west)
-            sem_wait(&sem_order);
-            west_pass++;
-            sem_wait(&screen);
-            printf("\x1b[0;34m\x1b[1m(...)West car arrive to the bridge [->] west=%d east=%d west_pass=%d thread=%ld\x1b[0m\n", west, east,west_pass, pthread_self());
-            sem_post(&screen);
-            sem_post(&sem_order);
-
-            if (bridge_direction == EAST_DIRECTION){
-            
-                sem_wait(&screen);
-                printf("\x1b[31;1m\x1b[1m(...)West waiting [->] west_pass=%d  thread=%ld\x1b[0m\n", west_pass, pthread_self());
-                sem_post(&screen);
-            
-            }
-
-
-            sem_wait(&sem_west);
-            
-            sem_wait(&sem_bridge);
-            if (bridge_direction == WEST_DIRECTION){
-             
-                sem_wait(&screen);
-                printf("\x1b[31;1m\x1b[1m(...)West (1) passing the bridge [->] west_pass=%d thread=%ld\x1b[0m\n", west_pass, pthread_self());
-                sem_post(&screen);
-            
-            } else if (bridge_direction == -1 || west_pass == 1){
-            
-                sem_wait(&sem_east);
-                bridge_direction = WEST_DIRECTION;
-                sem_wait(&screen);
-                printf("\x1b[31;1m\x1b[1m(...)West start to pass [->] west_pass=%d  thread=%ld\x1b[0m\n", west_pass, pthread_self());
-                sem_post(&screen);
-            
-            }
-            
-            sem_post(&sem_bridge);
         }
+
+        if (bridge_direction != EAST_DIRECTION && actual_east_pass == 1){
+            //Direccion opuesta libre, soy el primero
+
+            sem_wait(&screen);
+            fflush(stdin); 
+            sem_post(&screen);   
+
+            sem_wait(&sem_real_bridge);
+            bridge_direction = EAST_DIRECTION;
+            sem_wait(&screen);
+            printf("\x1b[31;1m\x1b[1m(...)East start to pass [<-] east_pass=%d  thread=%ld\x1b[0m\n", east_pass, pthread_self());
+            fflush(stdin); 
+            sem_post(&screen);   
+            pthread_cond_broadcast(&con_east);
+
+        }else if (bridge_direction != EAST_DIRECTION){ // Direccion opuesta y no es el primero
+            pthread_mutex_lock(&mut_east);
+            pthread_cond_wait(&con_east, &mut_east);//Esperamos a que el primero avance
+            pthread_mutex_unlock(&mut_east);
+        }
+
+        sem_wait(&screen);
+        printf("\x1b[31;1m\x1b[1m(...)East passing the bridge [<-] east_pass=%d thread=%ld\x1b[0m\n", east_pass, pthread_self());
+        fflush(stdin); 
+        sem_post(&screen);
+
+    } else {
+        //(WEST)
+        //Se espera a que los carros lleguen al puente.
+        sem_wait(&sem_order);
+        west_pass++;
+        actual_west_pass =  west_pass;
+        sem_wait(&screen);
+        printf("\x1b[0;34m\x1b[1m(...)West car arrive to the bridge [->] west=%d east=%d west_pass=%d thread=%ld\x1b[0m\n", west, east,west_pass, pthread_self());
+        fflush(stdin); 
+        sem_post(&screen);
+        sem_post(&sem_order);
+
+        if (bridge_direction == EAST_DIRECTION){ //El puente esta en direccion opuesta
+            sem_wait(&screen);
+            printf("\x1b[31;1m\x1b[1m(...)West waiting [->] west_pass=%d  thread=%ld\x1b[0m\n", west_pass, pthread_self());
+            fflush(stdin); 
+            sem_post(&screen);
+        }
+        
+        if (bridge_direction != WEST_DIRECTION && actual_west_pass == 1){
+            //Direccion opuesta libre, soy el primero
+
+            sem_wait(&screen);
+            fflush(stdin); 
+            sem_post(&screen);   
+
+            sem_wait(&sem_real_bridge);
+            bridge_direction = WEST_DIRECTION;
+            sem_wait(&screen);
+            printf("\x1b[31;1m\x1b[1m(...)West start to pass [->] west_pass=%d  thread=%ld\x1b[0m\n", west_pass, pthread_self());
+            fflush(stdin); 
+            sem_post(&screen);
+            pthread_cond_broadcast(&con_west);
+
+        }else if (bridge_direction != WEST_DIRECTION){ // Direccion opuesta y no es el primero
+            pthread_mutex_lock(&mut_west);
+            pthread_cond_wait(&con_west, &mut_west);
+            pthread_mutex_unlock(&mut_west);
+        }
+
+        sem_wait(&screen);
+        printf("\x1b[31;1m\x1b[1m(...)West passing the bridge [->] west_pass=%d thread=%ld\x1b[0m\n", west_pass, pthread_self());
+        fflush(stdin); 
+        sem_post(&screen);
+    }
 }
 
 void leave(int direction)
 {
-//Time in the bridge
+//Tiempo de ejecucion dentro del puento
 sleep(2);
 
-//if the car is going est
+//Puente es en dirreccion este
     if (EAST_DIRECTION == direction) {
-        sem_wait(&screen);
-        printf("\x1b[0;33m\x1b[1m(--)East car leave [<-] direction=%s east=%d west=%d thread=%ld\x1b[0m\n", name(direction), east, west, pthread_self());
-        sem_post(&screen);
-        sem_post(&sem_east);
-
         sem_wait(&sem_order);
-        --east;
-        --east_pass;
+        east--;
+        east_pass--;
+        sem_wait(&screen);
+        printf("\x1b[0;33m\x1b[1m(--)East car leave [<-] direction=%s east=%d west=%d east_pass=%d thread=%ld\x1b[0m\n", name(direction), east, west,east_pass, pthread_self());
+        fflush(stdin); 
+        sem_post(&screen);
         if (east_pass == 0){
-            sem_post(&sem_west);
+            sem_post(&sem_real_bridge);
             bridge_direction = -1;
+        }else{
+            pthread_cond_broadcast(&con_east);
         }
         sem_post(&sem_order);
     }
-//if the car is going est
+//Puente es en dirreccion oeste
     else if (WEST_DIRECTION == direction) {
-        sem_wait(&screen);
-        printf("\x1b[0;33m\x1b[1m(--)West car leave [->] direction=%s east=%d west=%d thread=%ld\x1b[0m\n", name(direction), east, west, pthread_self());
-        sem_post(&screen);
-        sem_post(&sem_west);
-
         sem_wait(&sem_order);
-        --west;
-        --west_pass;
+        west--;
+        west_pass--;
+        sem_wait(&screen);
+        printf("\x1b[0;33m\x1b[1m(--)West car leave [->] direction=%s east=%d west=%d west_pass=%d thread=%ld\x1b[0m\n", name(direction), east, west,west_pass, pthread_self());
+        fflush(stdin); 
+        sem_post(&screen);
         if (west_pass == 0){
-            sem_post(&sem_east);
+            sem_post(&sem_real_bridge);
             bridge_direction = -1;
+        }else{
+            pthread_cond_broadcast(&con_west);
         }
         sem_post(&sem_order);
     }
@@ -214,64 +247,60 @@ void *thread_creation(struct car_creation_thread *car_thread){
 }
 
 int main(int argc, char *argv[]) {
-     int WEST_THREAD_SIZE;
-     int EAST_THREAD_SIZE;
+   int WEST_THREAD_SIZE;
+   int EAST_THREAD_SIZE;
 
      /*If the number of arguments entered is less than or equal to 1 then,
      a number of random cars is defined in both directions*/
-    if (argc <= 1) {
+   if (argc <= 1) {
       printf("A random number of cars for both directions is set.\n");
       WEST_THREAD_SIZE = rand() % 6;
       EAST_THREAD_SIZE = rand() % 6;
       //east random number definition.
-    } else if(argc <= 2) {
+  } else if(argc <= 2) {
       printf("A random number of cars for heading east is set.\n");
       WEST_THREAD_SIZE = atoi(argv[1]);
       EAST_THREAD_SIZE = rand() % 6;
-    } else {
+  } else {
       //conversion of received arguments.
       WEST_THREAD_SIZE = atoi(argv[1]);
       EAST_THREAD_SIZE  = atoi(argv[2]);
-    }
+  }
 
-    printf("Quantity of cars heading west: %i\n", WEST_THREAD_SIZE);
-    printf("Quantity of cars heading east: %i\n", EAST_THREAD_SIZE);
+  printf("Quantity of cars heading west: %i\n", WEST_THREAD_SIZE);
+  printf("Quantity of cars heading east: %i\n", EAST_THREAD_SIZE);
 
-    sem_init(&sem_order, 0, 1);
-    sem_init(&sem_bridge, 0, 1);
-    sem_init(&sem_east, 0, 1);
-    sem_init(&sem_west, 0, 1);
-    sem_init(&screen, 0, 1);
+  sem_init(&sem_order, 0, 1);
+  sem_init(&sem_real_bridge, 0, 1);
+  sem_init(&screen, 0, 1);
 
-    pthread_t* thread_creation_list = malloc(sizeof(pthread_t) * 2);
-    int thread_total = EAST_THREAD_SIZE + WEST_THREAD_SIZE;
+  pthread_t* thread_creation_list = malloc(sizeof(pthread_t) * 2);
+  int thread_total = EAST_THREAD_SIZE + WEST_THREAD_SIZE;
 
-    srand(time(NULL));
+  srand(time(NULL));
 
     //structs definition.
-    struct car_creation_thread thread_west;
-    thread_west.direction = WEST_DIRECTION;
-    thread_west.size = WEST_THREAD_SIZE;
-    thread_west.medium = 0.4;
-    struct car_creation_thread thread_east;
-    thread_east.direction = EAST_DIRECTION;
-    thread_east.size = EAST_THREAD_SIZE;
-    thread_east.medium = 0.7;
+  struct car_creation_thread thread_west;
+  thread_west.direction = WEST_DIRECTION;
+  thread_west.size = WEST_THREAD_SIZE;
+  thread_west.medium = 0.4;
+  struct car_creation_thread thread_east;
+  thread_east.direction = EAST_DIRECTION;
+  thread_east.size = EAST_THREAD_SIZE;
+  thread_east.medium = 0.7;
 
-    pthread_create(&thread_creation_list[0], NULL, thread_creation, &thread_west);
-    pthread_create(&thread_creation_list[1], NULL, thread_creation, &thread_east);
-    pthread_join(thread_creation_list[0], NULL);
-    pthread_join(thread_creation_list[1], NULL);
-    
-    printf("\x1b[0m");
-    
-    sem_destroy(&screen);
-    sem_destroy(&sem_order);
-    sem_destroy(&sem_west);
-    sem_destroy(&sem_bridge);
-    sem_destroy(&sem_east);
-    free(thread_creation_list);
+  pthread_create(&thread_creation_list[0], NULL, thread_creation, &thread_west);
+  pthread_create(&thread_creation_list[1], NULL, thread_creation, &thread_east);
+  pthread_join(thread_creation_list[0], NULL);
+  pthread_join(thread_creation_list[1], NULL);
 
-    sleep(1);
-    exit(0);
+  printf("\x1b[0m");
+
+  sem_destroy(&screen);
+  sem_destroy(&sem_order);
+  sem_destroy(&sem_real_bridge);
+  free(thread_creation_list);
+
+  sleep(1);
+  exit(0);
 }
